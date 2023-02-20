cf216b35e146ebf712865ddf595bfcc3b600c2a7
Admin <Admin@DESKTOP-M7RVK46>
2023-02-20 20:37:58 +0100

    Recuenta = 9

--- src/controldeversionesv1/ControlDeVersionesV1.java
+++ src/controldeversionesv1/ControlDeVersionesV1.java
@@ -16,7 +16,7 @@ public class ControlDeVersionesV1 {
     public static void main(String[] args) {
         // TODO code application logic here
        Contador recuenta = new Contador();
-       recuenta.setCuenta(8);
+       recuenta.setCuenta(9);
        System.out.println("Recuenta= "+recuenta.getCuenta());
     }
     
