8c4ad8564079a73360d9eb4d9334a12b8c97b743
Admin <Admin@DESKTOP-M7RVK46>
2023-02-20 19:55:24 +0100

    Versión inicial. Valor = 1

---
+++ src/controldeversionesv1/ControlDeVersionesV1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package controldeversionesv1;
+
+/**
+ *
+ * @author Admin
+ */
+public class ControlDeVersionesV1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       int valor;
+       valor = 1;
+       System.out.println("Valor= "+valor);
+    }
+    
+}
